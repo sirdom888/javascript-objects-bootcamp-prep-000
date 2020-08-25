89d7995d9d60082e41479f76eebec3ed922e8adf
pleasant-calculator-7755 <domcode88@gmail.com>
2020-08-25 19:16:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
-var playlist = { artistName: "songTitle"};
+var playlist = { artistName: " "};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle; 
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
